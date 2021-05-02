80d6d48e9d9165231d540a06d1dfbbeb2576cb86
Abdulla <abdulla.alkamil@gmail.com>
2021-05-02 11:12:30 +0100

    balances and smart wallets

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,20 +1,32 @@
-import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  Contract,
+  BigInt,
+  Address
+} from "@graphprotocol/graph-ts"
+import {
+  LimitOrderBook,
   OrderChanged,
   OrderCreated,
   OrderFilled,
   TrailingOrderCreated,
   TrailingOrderFilled,
   PokeContractCall
-} from "../generated/Contract/Contract"
+} from "../generated/LimitOrderBook/LimitOrderBook"
+import {
+  SmartWalletFactory,
+  Created
+} from "../generated/SmartWalletFactory/SmartWalletFactory"
+import {
+  Transfer,
+  USDC
+} from "../generated/USDC/USDC"
 import { Order,
-  TrailingOrder
+  TrailingOrder,
+  SmartWallet
 } from "../generated/schema"
 
 export function handleOrderChanged(event: OrderChanged): void {
   let entity = Order.load(event.params.order_id.toString())
-  let contract = Contract.bind(event.address)
+  let contract = LimitOrderBook.bind(event.address)
   let order = contract.getLimitOrder(event.params.order_id)
   entity.reduceOnly = order.reduceOnly
   entity.stillValid = order.stillValid
@@ -30,7 +42,7 @@ export function handleOrderChanged(event: OrderChanged): void {
 
 export function handleOrderCreated(event: OrderCreated): void {
   let entity = new Order(event.params.order_id.toString())
-  let contract = Contract.bind(event.address)
+  let contract = LimitOrderBook.bind(event.address)
   let order = contract.getLimitOrder(event.params.order_id)
   entity.id = event.params.order_id.toString()
   entity.trader = event.params.trader
@@ -60,7 +72,7 @@ export function handleOrderFilled(event: OrderFilled): void {
 
 export function handleTrailingOrderCreated(event: TrailingOrderCreated): void {
   let entity = new TrailingOrder(event.params.order_id.toString())
-  let contract = Contract.bind(event.address)
+  let contract = LimitOrderBook.bind(event.address)
   let order = contract.getTrailingData(event.params.order_id)
   entity.witnessPrice = order.witnessPrice.d
   entity.trail = order.trail.d
@@ -77,7 +89,7 @@ export function handleTrailingOrderCreated(event: TrailingOrderCreated): void {
 
 export function handlePokeContract(call: PokeContractCall): void {
   let entity = TrailingOrder.load(call.inputs.order_id.toString())
-  let contract = Contract.bind(call.to)
+  let contract = LimitOrderBook.bind(call.to)
   let order = contract.getTrailingData(call.inputs.order_id)
   entity.witnessPrice = order.witnessPrice.d
   entity.trail = order.trail.d
@@ -93,3 +105,26 @@ export function handlePokeContract(call: PokeContractCall): void {
 }
 
 export function handleTrailingOrderFilled(event: TrailingOrderFilled): void {}
+
+export function handleCreated(event: Created): void {
+  let entity = new SmartWallet(event.params.smartWallet.toHexString())
+  entity.balance = BigInt.fromI32(0)
+  entity.blockCreated = event.block.number
+  entity.tradeCount = BigInt.fromI32(0)
+  entity.owner = event.params.owner
+  entity.save()
+}
+
+export function handleTransfer(event: Transfer): void {
+  let wallet = SmartWallet.load(event.params.from.toHexString())
+  if(wallet == null) {
+    wallet = SmartWallet.load(event.params.to.toHexString())
+    if(wallet == null) {
+      //do nothing
+    } else {
+      let contract = USDC.bind(event.address)
+      wallet.balance = contract.balanceOf(Address.fromString(wallet.id))
+      wallet.save()
+    }
+  }
+}
