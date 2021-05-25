483d5885f6aab3d6459a517cc6fe6290cd1d8a96
Abdulla <abdulla.alkamil@gmail.com>
2021-05-25 21:57:09 +0100

    updated for new contracts

--- src/mapping.ts
+++ src/mapping.ts
@@ -6,47 +6,18 @@ import {
   OrderChanged,
   OrderCreated,
   OrderFilled,
+  OrderCancelled,
   TrailingOrderCreated,
   TrailingOrderFilled,
-  PokeContractCall
+  TrailingOrderChanged,
+  TrailingOrderCancelled,
+  ContractPoked
 } from "../generated/LimitOrderBook/LimitOrderBook"
 import {
   Order,
   TrailingOrder,
   Trade
 } from "../generated/schema"
-
-export function handleOrderChanged(event: OrderChanged): void {
-  let entity = Order.load(event.params.order_id.toString())
-  let contract = LimitOrderBook.bind(event.address)
-  let order = contract.getLimitOrder(event.params.order_id)
-  entity.reduceOnly = order.reduceOnly
-  entity.stillValid = order.stillValid
-  entity.stopPrice = order.stopPrice.d
-  entity.limitPrice = order.limitPrice.d
-  entity.orderSize = order.orderSize.d
-  entity.collateral = order.collateral.d
-  entity.leverage = order.leverage.d
-  entity.slippage = order.slippage.d
-  entity.expiry = order.expiry
-  entity.save()
-
-  let trailingEntity = TrailingOrder.load(event.params.order_id.toString())
-  if(trailingEntity == null) {
-    return
-  }
-  let to = contract.getTrailingData(event.params.order_id)
-  trailingEntity.witnessPrice = to.witnessPrice.d
-  trailingEntity.trail = to.trail.d
-  trailingEntity.trailPct = to.trailPct.d
-  trailingEntity.gap = to.gap.d
-  trailingEntity.gapPct = to.gapPct.d
-  trailingEntity.snapshotLastUpdated = to.snapshotLastUpdated
-  trailingEntity.snapshotTimestamp = to.snapshotTimestamp
-  trailingEntity.lastUpdatedKeeper = to.lastUpdatedKeeper
-  trailingEntity.usePct = to.usePct
-  trailingEntity.save()
-}
 
 export function handleOrderCreated(event: OrderCreated): void {
   let entity = new Order(event.params.order_id.toString())
@@ -70,7 +41,6 @@ export function handleOrderCreated(event: OrderCreated): void {
   entity.save()
 }
 
-
 export function handleOrderFilled(event: OrderFilled): void {
   let entity = Order.load(event.params.order_id.toString())
   entity.filled = true
@@ -83,6 +53,28 @@ export function handleOrderFilled(event: OrderFilled): void {
   trade.order_id = event.params.order_id
   trade.order = event.params.order_id.toString()
   trade.save()
+}
+
+export function handleOrderChanged(event: OrderChanged): void {
+  let entity = Order.load(event.params.order_id.toString())
+  let contract = LimitOrderBook.bind(event.address)
+  let order = contract.getLimitOrder(event.params.order_id)
+  entity.reduceOnly = order.reduceOnly
+  entity.stillValid = order.stillValid
+  entity.stopPrice = order.stopPrice.d
+  entity.limitPrice = order.limitPrice.d
+  entity.orderSize = order.orderSize.d
+  entity.collateral = order.collateral.d
+  entity.leverage = order.leverage.d
+  entity.slippage = order.slippage.d
+  entity.expiry = order.expiry
+  entity.save()
+}
+
+export function handleOrderCancelled(event: OrderCancelled): void {
+  let entity = Order.load(event.params.order_id.toString())
+  entity.stillValid = false
+  entity.save()
 }
 
 export function handleTrailingOrderCreated(event: TrailingOrderCreated): void {
@@ -100,12 +92,40 @@ export function handleTrailingOrderCreated(event: TrailingOrderCreated): void {
   entity.lastUpdatedKeeper = order.lastUpdatedKeeper
   entity.usePct = order.usePct
   entity.save()
+
+  let orderData = Order.load(event.params.order_id.toString())
+  orderData.trailingData = event.params.order_id.toString()
+  orderData.save()
 }
 
-export function handlePokeContract(call: PokeContractCall): void {
-  let entity = TrailingOrder.load(call.inputs.order_id.toString())
-  let contract = LimitOrderBook.bind(call.to)
-  let order = contract.getTrailingData(call.inputs.order_id)
+export function handleTrailingOrderFilled(event: TrailingOrderFilled): void {
+}
+
+export function handleTrailingOrderChanged(event: TrailingOrderChanged): void {
+  let trailingEntity = TrailingOrder.load(event.params.order_id.toString())
+  let contract = LimitOrderBook.bind(event.address)
+  let to = contract.getTrailingData(event.params.order_id)
+  trailingEntity.witnessPrice = to.witnessPrice.d
+  trailingEntity.trail = to.trail.d
+  trailingEntity.trailPct = to.trailPct.d
+  trailingEntity.gap = to.gap.d
+  trailingEntity.gapPct = to.gapPct.d
+  trailingEntity.snapshotLastUpdated = to.snapshotLastUpdated
+  trailingEntity.snapshotTimestamp = to.snapshotTimestamp
+  trailingEntity.lastUpdatedKeeper = to.lastUpdatedKeeper
+  trailingEntity.usePct = to.usePct
+  trailingEntity.save()
+}
+
+export function handleTrailingOrderCancelled(event: TrailingOrderCancelled): void {
+
+}
+
+
+export function handleContractPoked(event: ContractPoked): void {
+  let entity = TrailingOrder.load(event.params.order_id.toString())
+  let contract = LimitOrderBook.bind(event.address)
+  let order = contract.getTrailingData(event.params.order_id)
   entity.witnessPrice = order.witnessPrice.d
   entity.trail = order.trail.d
   entity.trailPct = order.trailPct.d
@@ -118,11 +138,9 @@ export function handlePokeContract(call: PokeContractCall): void {
   entity.usePct = order.usePct
   entity.save()
 
-  let ent = Order.load(call.inputs.order_id.toString())
-  let ord = contract.getLimitOrder(call.inputs.order_id)
+  let ent = Order.load(event.params.order_id.toString())
+  let ord = contract.getLimitOrder(event.params.order_id)
   ent.stopPrice = ord.stopPrice.d
   ent.limitPrice = ord.limitPrice.d
   ent.save()
 }
-
-export function handleTrailingOrderFilled(event: TrailingOrderFilled): void {}
